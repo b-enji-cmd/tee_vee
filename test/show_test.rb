628bac2d6f6accfb3ba9ae2aa1891c3b24efa302
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-15 09:14:59 -0700

    Add instance and attribute tests for show

---
+++ test/show_test.rb
@@ -0,0 +1,22 @@
+require './lib/character'
+require './lib/show'
+require 'minitest/autorun'
+require 'pry'
+
+class ShowTest < Minitest::Test
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_is
+    assert_instance_of Show, @knight_rider
+  end
+
+  def test_it_has_things
+    assert_equal "Knight Rider", @knight_rider.name
+    assert_equal "Glen Larson", @knight_rider.creator
+    assert_equal [@michael_knight, @kitt], @knight_rider.characters
+  end
+end
