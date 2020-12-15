bb171da80832bbf239fc6f90c8814d4e56a97d2f
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-15 09:19:00 -0700

    Add total salary and highest paid tests

--- test/show_test.rb
+++ test/show_test.rb
@@ -19,4 +19,12 @@ class ShowTest < Minitest::Test
     assert_equal "Glen Larson", @knight_rider.creator
     assert_equal [@michael_knight, @kitt], @knight_rider.characters
   end
+
+  def test_total_salary
+    assert_equal 2600000, @knight_rider.total_salary
+  end
+
+  def test_highest_paid_actor
+    assert_equal "David Hasselhoff", @knight_rider.highest_paid_actor
+  end
 end
