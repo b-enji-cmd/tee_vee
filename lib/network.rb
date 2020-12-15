7388e4ed072fa473340cf3c38b6ab23ab39be7a0
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-15 09:53:38 -0700

    Add main character indexing

--- lib/network.rb
+++ lib/network.rb
@@ -10,4 +10,12 @@ class Network
   def add_show(show)
     @shows << show
   end
+
+  def main_characters
+    @shows.flat_map do |show|
+      show.characters.select do |character|
+        character.name == character.name.upcase
+      end
+    end
+  end
 end
